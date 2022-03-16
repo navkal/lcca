a737912b5fecdf731adb6883508f9bd6bbac929e
navkal <navkal@hotmail.com>
2022-03-16 19:19:09 -0400

    Initial

--- faq/download_pdf.php
+++ faq/download_pdf.php
@@ -1,5 +1,5 @@
 <?php
-  // Copyright 2020 Energize Andover.  All rights reserved.
+  // Copyright 2022 Energize Lawrence.  All rights reserved.
 
   require_once $_SERVER["DOCUMENT_ROOT"]."/../common/util.php";
   downloadFile( 'andover_cca.pdf', '', 'pdf' );
--- util/format_event.php
+++ util/format_event.php
@@ -1,5 +1,5 @@
 <?php
-  // Copyright 2020 Energize Andover.  All rights reserved.
+  // Copyright 2022 Energize Lawrence.  All rights reserved.
 
   $aEvents =
   [
