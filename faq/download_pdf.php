c5fb0a3d266999a49c88530759701ae10819e3e7
navkal <navkal@hotmail.com>
2022-03-16 19:23:30 -0400

    Initial

--- faq/download_pdf.php
+++ faq/download_pdf.php
@@ -2,5 +2,5 @@
   // Copyright 2022 Energize Lawrence.  All rights reserved.
 
   require_once $_SERVER["DOCUMENT_ROOT"]."/../common/util.php";
-  downloadFile( 'andover_cca.pdf', '', 'pdf' );
+  downloadFile( 'lawrence_cca.pdf', '', 'pdf' );
 ?>
