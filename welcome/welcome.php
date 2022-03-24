4d1a866826e36330a930c1d162d63bf0664ed378
navkal <navkal@hotmail.com>
2022-03-24 13:58:57 -0400

    Initial

--- welcome/welcome.php
+++ welcome/welcome.php
@@ -112,7 +112,7 @@
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-size: cover;
-    overscroll-behavior: none;
+    background-color: red;
   }
 
   .jumbotron
