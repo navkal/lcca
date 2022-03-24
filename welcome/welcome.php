cdc7663d4c46cd7dbb03d2311250ae09c719726a
navkal <navkal@hotmail.com>
2022-03-24 13:57:54 -0400

    Initial

--- welcome/welcome.php
+++ welcome/welcome.php
@@ -112,7 +112,7 @@
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-size: cover;
-    overscroll-behavior: contain;
+    overscroll-behavior: none;
   }
 
   .jumbotron
